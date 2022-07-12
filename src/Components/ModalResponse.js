d343bb506956f2fe36f079126b8e8abb73d6358a
arif.althia <arif.althia@shopee.com>
2022-07-12 10:23:43 +0700

    refactor: extract modal into component

---
+++ src/Components/ModalResponse.js
@@ -0,0 +1,115 @@
+import { Link } from 'react-router-dom';
+
+function ModalResponse(props) {
+  const { status, modalHeader, dataModal } = props;
+  return (
+    <div>
+      {
+                status === 200
+                  ? (
+                    <div
+                      className="modal fade"
+                      id="staticBackdrop"
+                      data-bs-backdrop="static"
+                      data-bs-keyboard="false"
+                      tabIndex="-1"
+                      aria-labelledby="staticBackdropLabel"
+                      aria-hidden="true"
+                    >
+                      <div className="modal-dialog">
+                        <div className="modal-content">
+                          <div className="modal-header" style={{ justifyContent: 'center' }}>
+                            <h5 className="modal-title" id="staticBackdropLabel">{modalHeader.type}</h5>
+                          </div>
+                          <h2 className="text-success">{modalHeader.message}</h2>
+                          <div className="modal-body" style={{ textAlign: 'left' }}>
+                            <div className="row justify-content-between">
+                              <div className="col-6">
+                                <p>Amount</p>
+                              </div>
+                              <div className="col-6" style={{ textAlign: 'right' }}>
+                                <h4>{dataModal.amount}</h4>
+                              </div>
+                            </div>
+                            <div className="row justify-content-between">
+                              <div className="col-6">
+                                <p>Transaction ID</p>
+                              </div>
+                              <div className="col-6" style={{ textAlign: 'right' }}>
+                                <p>{dataModal.id}</p>
+                              </div>
+                            </div>
+                            <div className="row justify-content-between">
+                              <div className="col-6">
+                                <p>From</p>
+                              </div>
+                              <div className="col-6" style={{ textAlign: 'right' }}>
+                                <p>{dataModal.sourceWalletID}</p>
+                              </div>
+                            </div>
+                            <div className="row justify-content-between">
+                              <div className="col-6">
+                                <p>To</p>
+                              </div>
+                              <div className="col-6" style={{ textAlign: 'right' }}>
+                                <p>{dataModal.destinationWalletID}</p>
+                              </div>
+                            </div>
+                            <div className="row justify-content-between">
+                              <div className="col-6">
+                                <p>Description</p>
+                              </div>
+                              <div className="col-6" style={{ textAlign: 'right' }}>
+                                <p>{dataModal.description}</p>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
+                              <Link to="/" className="text-white" style={{ textDecoration: 'none' }}>Close</Link>
+                            </button>
+                            <button type="button" className="btn btn-primary" data-bs-dismiss="modal" data-bs-target="staticBackdrop">
+                              <Link to="/" className="text-white" style={{ textDecoration: 'none' }}>Print</Link>
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  )
+                  : (
+                    <div
+                      className="modal fade"
+                      id="staticBackdrop"
+                      data-bs-backdrop="static"
+                      data-bs-keyboard="false"
+                      tabIndex="-1"
+                      aria-labelledby="staticBackdropLabel"
+                      aria-hidden="true"
+                    >
+                      <div className="modal-dialog">
+                        <div className="modal-content">
+                          <div className="modal-header">
+                            <h5 className="modal-title" id="staticBackdropLabel" style={{ textAlign: 'center' }}>
+                              {`${modalHeader.type} Failed`}
+                            </h5>
+                          </div>
+                          <div className="modal-body text-danger">
+                            Check Your Transaction Again
+                          </div>
+                          <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
+                              <Link to="/" className="text-white" style={{ textDecoration: 'none' }}>Close</Link>
+                            </button>
+                            <button type="button" className="btn btn-primary" data-bs-dismiss="modal" data-bs-target="staticBackdrop">
+                              <Link to="/" className="text-white" style={{ textDecoration: 'none' }}>Print</Link>
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  )
+            }
+    </div>
+  );
+}
+export default ModalResponse;
--- src/Components/topupPages/TopupInput.js
+++ src/Components/topupPages/TopupInput.js
@@ -1,9 +1,13 @@
+import ModalResponse from '../ModalResponse';
+
 function TopupInput(props) {
   const {
     submitTopup, topup, handleChange, status, dataModal,
   } = props;
-
-  console.log(dataModal);
+  const modalHeader = {
+    type: 'Top Up',
+    message: 'Top Up Success',
+  };
   return (
     <>
       <div className="row-12">
@@ -31,62 +35,10 @@ function TopupInput(props) {
               <input className="form-control" id="amountTopup" type="number" placeholder="0" name="amount" value={topup.amount} onChange={handleChange} />
             </label>
           </div>
-          <input type="submit" className="btn btn-primary form-control w-50" data-bs-toggle="modal" data-bs-target="#exampleModal" value="Top Up" />
+          <input type="submit" className="btn btn-primary form-control w-50" data-bs-toggle="modal" data-bs-target="#staticBackdrop" value="Top Up" />
         </form>
       </div>
-      {
-        status === 200
-          ? (
-            <div
-              className="modal fade"
-              id="exampleModal"
-              tabIndex="-1"
-              aria-labelledby="exampleModalLabel"
-              aria-hidden="true"
-            >
-              <div className="modal-dialog">
-                <div className="modal-content">
-                  <div className="modal-header">
-                    <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
-                  </div>
-                  <div className="modal-body">
-                    Success
-                  </div>
-                  <div className="modal-footer">
-                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" className="btn btn-primary">Save changes</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          )
-          : (
-            <div
-              className="modal fade"
-              id="exampleModal"
-              tabIndex="-1"
-              aria-labelledby="exampleModalLabel"
-              aria-hidden="true"
-            >
-              <div className="modal-dialog">
-                <div className="modal-content">
-                  <div className="modal-header">
-                    <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
-                  </div>
-                  <div className="modal-body">
-                    Error
-                  </div>
-                  <div className="modal-footer">
-                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" className="btn btn-primary">Save changes</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          )
-      }
+      <ModalResponse dataModal={dataModal} status={status} modalHeader={modalHeader} />
     </>
   );
 }
