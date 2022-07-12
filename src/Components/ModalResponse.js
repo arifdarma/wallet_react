9f8fe8af59ce263a2173e318078ade8cf024f9d2
arif.althia <arif.althia@shopee.com>
2022-07-12 18:15:11 +0700

    refactor: add check icon when success

--- src/Components/ModalResponse.js
+++ src/Components/ModalResponse.js
@@ -4,6 +4,13 @@ function ModalResponse(props) {
   const {
     status, modalHeader, dataModal, error,
   } = props;
+  const getBalance = (balance) => {
+    const formatter = new Intl.NumberFormat('id-ID', {
+      style: 'currency',
+      currency: 'IDR',
+    });
+    return formatter.format(balance).replace('Rp', '');
+  };
   return (
     <div>
       {
@@ -23,6 +30,12 @@ function ModalResponse(props) {
                           <div className="modal-header" style={{ justifyContent: 'center' }}>
                             <h5 className="modal-title" id="staticBackdropLabel">{modalHeader.type}</h5>
                           </div>
+                          <div className="my-1" style={{ textAlign: 'center' }}>
+                            <svg width="63" height="63" viewBox="0 0 63 63" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <circle cx="31.5" cy="31.5" r="31.5" fill="#2DC071" />
+                              <path d="M49 20L27.1563 42.5199C26.3706 43.33 25.0708 43.33 24.2851 42.5199L15 32.9474" stroke="white" strokeWidth="6" strokeLinecap="round" />
+                            </svg>
+                          </div>
                           <h2 className="text-success" style={{ textAlign: 'center' }}>{modalHeader.message}</h2>
                           <div className="modal-body" style={{ textAlign: 'left' }}>
                             <div className="row justify-content-between">
@@ -30,7 +43,7 @@ function ModalResponse(props) {
                                 <p>Amount</p>
                               </div>
                               <div className="col-6" style={{ textAlign: 'right' }}>
-                                <h4>{dataModal.amount}</h4>
+                                <h4>{getBalance(dataModal.amount)}</h4>
                               </div>
                             </div>
                             <div className="row justify-content-between">
